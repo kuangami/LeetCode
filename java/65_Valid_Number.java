3b548cc563795a3eb1569b5f166a602f5417d7ff
kuangami <david.qinkuang@gmail.com>
2017-01-12 13:37:31 -0600

    Fix tab issue

--- java/65_Valid_Number.java
+++ java/65_Valid_Number.java
@@ -20,36 +20,36 @@
  */
 public class Solution {
     public boolean isNumber(String s) {
-    	boolean e = false;
-    	boolean dot = false;
-    	boolean number = false;
-    	boolean numberAfterE = false;
+        boolean e = false;
+        boolean dot = false;
+        boolean number = false;
+        boolean numberAfterE = false;
 
         s = s.trim();
         
         for (int i = 0; i < s.length(); i++) {
-        	if (s.charAt(i) == '-' || s.charAt(i) == '+') {
-        	    if (i != 0 && s.charAt(i - 1) != 'e')
-        	        return false;
-        	}
-        	else if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-        	    number = true;
-        	    numberAfterE = true;
-        	}
-        	else if (s.charAt(i) == 'e') {
-        	    if (e || !number)
-        	        return false;
-        	    e = true;
-        	    numberAfterE = false;
-        	}
-        	else if (s.charAt(i) == '.') {
-        	    if (e || dot)
-        	        return false;
-    		dot = true;
-        	}
-        	else 
-        	    return false;
-		
+            if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+                if (i != 0 && s.charAt(i - 1) != 'e')
+                    return false;
+            }
+            else if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                number = true;
+                numberAfterE = true;
+            }
+            else if (s.charAt(i) == 'e') {
+                if (e || !number)
+                    return false;
+                e = true;
+                numberAfterE = false;
+            }
+            else if (s.charAt(i) == '.') {
+                if (e || dot)
+                    return false;
+                dot = true;
+            }
+            else 
+                return false;
+        
             }
         return number && numberAfterE;
     }
@@ -57,14 +57,14 @@ public class Solution {
     public static void main(String[] args) {
         String s1 = "1", s2 = " +1", s3 = "2s2e342",
                 s4 = " +-1 ", s5 = "2.4", s6 = ".4", s7 = "2e4", s8 = "-12e-10";
-    	Solution sol = new Solution();
-    	System.out.println(sol.isNumber(s1));
-    	System.out.println(sol.isNumber(s2));
-    	System.out.println(sol.isNumber(s3));
-    	System.out.println(sol.isNumber(s4));
-    	System.out.println(sol.isNumber(s5));
-    	System.out.println(sol.isNumber(s6));
-    	System.out.println(sol.isNumber(s7));
-    	System.out.println(sol.isNumber(s8));
+        Solution sol = new Solution();
+        System.out.println(sol.isNumber(s1));
+        System.out.println(sol.isNumber(s2));
+        System.out.println(sol.isNumber(s3));
+        System.out.println(sol.isNumber(s4));
+        System.out.println(sol.isNumber(s5));
+        System.out.println(sol.isNumber(s6));
+        System.out.println(sol.isNumber(s7));
+        System.out.println(sol.isNumber(s8));
     }
 }
