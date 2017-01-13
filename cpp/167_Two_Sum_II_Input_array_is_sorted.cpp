76c618c9a08ec120f23ecda725baffa01ea83750
kuangami <david.qinkuang@gmail.com>
2017-01-13 16:47:47 -0600

    Fix comment on the title

--- cpp/167_Two_Sum_II_Input_array_is_sorted.cpp
+++ cpp/167_Two_Sum_II_Input_array_is_sorted.cpp
@@ -1,4 +1,4 @@
-167. Two Sum II - Input array is sorted
+// 167. Two Sum II - Input array is sorted
 /**
  * Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
  * 
